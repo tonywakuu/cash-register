e56afad8cdf123902a466685cf85ec4190d907d8
Tony Walker <38742550+tonywakuu@users.noreply.github.com>
2020-05-05 16:31:17 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'))
 app.use(router)
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 3000)
